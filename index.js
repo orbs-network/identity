ec5be47a5cd8e5c81af7c8db898eeaf59d118e44
Kirill Maksimov <kirill@orbs.com>
2019-11-05 15:32:20 +0200

    Google OAuth2

---
+++ index.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const CALLBACK_URL = process.env.CALLBACK_URL || "http://localhost:8000/auth/google/callback";
+const PASSPORT_SESSION_SECRET = process.env.PASSPORT_SESSION_SECRET || "keyboard cat";
+
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: CALLBACK_URL,
+}, (accessToken, refreshToken, profile, cb) => {
+    console.log(profile._json)
+    cb(undefined, profile._json);
+}));
+
+const app = express();
+
+app.use(require("cookie-parser")());
+app.use(require("body-parser").urlencoded({ extended: true }));
+app.use(require("express-session")({ secret: PASSPORT_SESSION_SECRET, resave: true, saveUninitialized: true }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get("/auth/google",
+    passport.authenticate("google", { scope: ["profile", "email"] }));
+
+app.get("/auth/google/callback",
+    passport.authenticate("google", { failureRedirect: "/login" }),
+    (req, res) => {
+        // Successful authentication, redirect home.
+        res.redirect("/");
+    });
+
+app.get("/", (req, res) => {
+    res.send(req.user);
+});
+
+app.listen(process.env['PORT'] || 8000);
